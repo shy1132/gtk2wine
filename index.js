aa6befc90576681c08bb62eb508bbd156f34bf0a
shy <70299052+whakama@users.noreply.github.com>
2023-01-17 22:25:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //requires
 const fs = require('fs')
+const os = require('os')
+
+//variables
+var homeDir = os.homedir()
+var gtkRegMappings = {
+    
+}
 
 //code
-var gtk3Theme = fs.readFileSync('~/.config/gtk-3.0/colors.css')
+var gtk3ThemeCss = fs.readFileSync(`${homeDir}/.config/gtk-3.0/colors.css`).toString()
+var gtk3ThemeSplit = gtk3ThemeCss.split('\n')
+var gtk3ThemeParsed = {}
+for (let i = 0; i < gtk3ThemeSplit.length; i++) {
+    
+}
 
-console.log(gtk3Theme)
+console.log(gtk3ThemeParsed)
