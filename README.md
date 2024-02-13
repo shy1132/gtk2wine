9773baa66ff1440dc7542e0c14a14e1a2296818a
shy <70299052+whakama@users.noreply.github.com>
2024-02-13 03:29:20 -0600

    cleaned up and open sourced

---
+++ README.md
@@ -0,0 +1,9 @@
+# gtk2wine
+
+simple utility that converts kde gtk theme to a .reg file for use with theming wine to fit in with other applications
+
+its not perfect, but it works pretty well  
+to apply the output.reg file to wine, you simply run this: `wine regedit output.reg` in the current directory  
+if it doesn't work with output.reg, that's wine's fault just keep trying
+
+ignore sporadic commits from 2023 i wasnt as experienced in using github
--- index.js
+++ index.js
@@ -1,69 +1,71 @@
+//this is old-ish code from early 2023, assumes a lot of things but it 100% works in kde plasma
+
 //requires
 const fs = require('fs')
 const os = require('os')
 
 //variables
-var homeDir = os.homedir()
-var gtkRegMappings = {
-    "ActiveBorder": "theme_bg_color",
-    "ActiveTitle": "theme_bg_color",
-    "AppWorkSpace": "60 64 72",
-    "Background": "theme_base_color",
-    "ButtonAlternateFace": "theme_button_background_normal",
-    "ButtonDkShadow": "borders",
-    "ButtonFace": "theme_bg_color",
-    "ButtonHilight": "borders",
-    "ButtonLight": "theme_button_background_normal",
-    "ButtonShadow": "theme_bg_color",
-    "ButtonText": "theme_button_foreground_normal",
-    "GradientActiveTitle": "theme_bg_color",
-    "GradientInactiveTitle": "theme_bg_color",
-    "GrayText": "128 128 128",
-    "Hilight": "theme_selected_bg_color",
-    "HilightText": "theme_selected_fg_color",
-    "HotTrackingColor": "0 0 200",
-    "InactiveBorder": "theme_bg_color",
-    "InactiveTitle": "theme_bg_color",
-    "InactiveTitleText": "theme_unfocused_text_color",
-    "InfoText": "theme_text_color",
-    "InfoWindow": "theme_bg_color",
-    "Menu": "theme_bg_color",
-    "MenuBar": "theme_bg_color",
-    "MenuHilight": "theme_selected_bg_color",
-    "MenuText": "theme_text_color",
-    "Scrollbar": "73 78 88",
-    "TitleText": "theme_titlebar_foreground",
-    "Window": "theme_bg_color",
-    "WindowFrame": "theme_bg_color",
-    "WindowText": "theme_text_color"
+const homeDir = os.homedir()
+const gtkRegMappings = {
+    'ActiveBorder': 'theme_bg_color',
+    'ActiveTitle': 'theme_bg_color',
+    'AppWorkSpace': '60 64 72',
+    'Background': 'theme_base_color',
+    'ButtonAlternateFace': 'theme_button_background_normal',
+    'ButtonDkShadow': 'borders',
+    'ButtonFace': 'theme_bg_color',
+    'ButtonHilight': 'borders',
+    'ButtonLight': 'theme_button_background_normal',
+    'ButtonShadow': 'theme_bg_color',
+    'ButtonText': 'theme_button_foreground_normal',
+    'GradientActiveTitle': 'theme_bg_color',
+    'GradientInactiveTitle': 'theme_bg_color',
+    'GrayText': '128 128 128',
+    'Hilight': 'theme_selected_bg_color',
+    'HilightText': 'theme_selected_fg_color',
+    'HotTrackingColor': '0 0 200',
+    'InactiveBorder': 'theme_bg_color',
+    'InactiveTitle': 'theme_bg_color',
+    'InactiveTitleText': 'theme_unfocused_text_color',
+    'InfoText': 'theme_text_color',
+    'InfoWindow': 'theme_bg_color',
+    'Menu': 'theme_bg_color',
+    'MenuBar': 'theme_bg_color',
+    'MenuHilight': 'theme_selected_bg_color',
+    'MenuText': 'theme_text_color',
+    'Scrollbar': '73 78 88',
+    'TitleText': 'theme_titlebar_foreground',
+    'Window': 'theme_bg_color',
+    'WindowFrame': 'theme_bg_color',
+    'WindowText': 'theme_text_color'
 }
 
 //code
 function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return `${parseInt(result[1], 16)} ${parseInt(result[2], 16)} ${parseInt(result[3], 16)}`
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    return `${parseInt(result[1], 16)} ${parseInt(result[2], 16)} ${parseInt(result[3], 16)}`;
 }
 
-var gtk3ThemeCss = fs.readFileSync(`${homeDir}/.config/gtk-3.0/colors.css`).toString().trim().split('\n')
-var gtk3ThemeJSON = {}
-var regFile = `Windows Registry Editor Version 5.00\r\n\r\n[HKEY_CURRENT_USER\\Control Panel\\Colors]`
+let gtk3ThemeCss = fs.readFileSync(`${homeDir}/.config/gtk-3.0/colors.css`).toString().trim().split('\n')
+let gtk3ThemeJSON = {}
+let regFile = `Windows Registry Editor Version 5.00\r\n\r\n[HKEY_CURRENT_USER\\Control Panel\\Colors]`
 
 for (let i = 0; i < gtk3ThemeCss.length; i++) {
     gtk3ThemeCss[i] = gtk3ThemeCss[i].split('@define-color ')[1]
 
-    var attribute = gtk3ThemeCss[i].slice(0, gtk3ThemeCss[i].lastIndexOf('_'))
-    var value = gtk3ThemeCss[i].split(' ')[1].slice(1, -1)
+    let attribute = gtk3ThemeCss[i].slice(0, gtk3ThemeCss[i].lastIndexOf('_'))
+    let value = gtk3ThemeCss[i].split(' ')[1].slice(1, -1)
 
     gtk3ThemeJSON[attribute] = value
 }
 
-var regMappingsArr = Object.keys(gtkRegMappings)
+let regMappingsArr = Object.keys(gtkRegMappings)
 
 for (let i = 0; i < regMappingsArr.length; i++) {
-    var winAttribute = regMappingsArr[i]
+    let winAttribute = regMappingsArr[i]
 
-    var gtkAttribute = gtkRegMappings[winAttribute]
-    var gtkAttributeValue = !gtkAttribute.includes(' ') ? hexToRgb(gtk3ThemeJSON[gtkAttribute]) : gtkAttribute
+    let gtkAttribute = gtkRegMappings[winAttribute]
+    let gtkAttributeValue = !gtkAttribute.includes(' ') ? hexToRgb(gtk3ThemeJSON[gtkAttribute]) : gtkAttribute
 
     regFile += `\r\n"${winAttribute}"="${gtkAttributeValue}"`
 }
